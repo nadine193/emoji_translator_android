a129d7214bc2eeeb79eadfb44c8dbd2c73e883cf
nadine193 <nadine.el-refee@student.manchester.ac.uk>
2020-03-06 04:53:35 +0000

    connect to service call service when button is clicked. not fully working yet

--- src/main/java/com/example/emojitranslator/MainActivity.java
+++ src/main/java/com/example/emojitranslator/MainActivity.java
@@ -10,14 +10,23 @@ import androidx.core.provider.FontRequest;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TextInputLayout inputField;
     private TextInputLayout outputField;
+
+    private Button translateButton;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -34,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
         inputField = findViewById(R.id.inputTextField);
         outputField = findViewById(R.id.outputTextField);
+        translateButton = (Button)findViewById(R.id.translateButton);
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String userInput = inputField.getEditText().toString();
+                String myUrl = "http://localhost:8080/";
+                HttpGetRequest getRequest = new HttpGetRequest();
+                try {
+                    String result = getRequest.execute(myUrl, userInput).get();
+                    // fix this line, cant setText to textinputlayout
+                    outputField.setText(result);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
 
     }
 
