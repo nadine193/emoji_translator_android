9b230d7b9b9e08d74cb9bf27da3b1d5c47bdffae
nadine193 <nadine.el-refee@student.manchester.ac.uk>
2020-03-08 11:34:14 +0000

    UI connects to server Translates from emoji icon to description

--- src/main/java/com/example/emojitranslator/MainActivity.java
+++ src/main/java/com/example/emojitranslator/MainActivity.java
@@ -1,30 +1,30 @@
 package com.example.emojitranslator;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.provider.FontRequest;
-
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.concurrent.ExecutionException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TextInputLayout inputField;
-    private TextInputLayout outputField;
+    private EditText outputField;
 
     private Button translateButton;
 
@@ -48,20 +48,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                String userInput = inputField.getEditText().toString();
-                String myUrl = "http://localhost:8080/";
-                HttpGetRequest getRequest = new HttpGetRequest();
-                try {
-                    String result = getRequest.execute(myUrl, userInput).get();
-                    // fix this line, cant setText to textinputlayout
-                    outputField.setText(result);
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
 
+                String input = inputField.getEditText().getText().toString();
+                String url = "http://10.0.2.2:8080/getDescription?emojiIcon=" + input;
+                new MyTask().execute(url);
+
+            }
         });
 
     }
@@ -71,5 +63,33 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private class MyTask extends AsyncTask<String, Void, String> {
+        String result;
+
+        @Override
+        protected String doInBackground(String... urls) {
+            URL url;
+            try {
+                url = new URL(urls[0]);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+                String stringBuffer;
+                String string = "";
+                while ((stringBuffer = bufferedReader.readLine()) != null){
+                    string = String.format("%s%s", string, stringBuffer);
+                }
+                bufferedReader.close();
+                result = string;
+            } catch (IOException e){
+                e.printStackTrace();
+                result = e.toString();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            outputField.setText(result);
+        }
     }
 }
