22f43c01ef6a299f69ea1f2801dba5826bc5ea59
nadine193 <nadine.el-refee@student.manchester.ac.uk>
2020-03-09 03:16:47 +0000

    output box uses text to speech

--- src/main/java/com/example/emojitranslator/MainActivity.java
+++ src/main/java/com/example/emojitranslator/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.emojitranslator;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,18 +21,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TextInputLayout inputField;
     private EditText outputField;
-
     private Button translateButton;
+    private ToggleButton switchButton;
+    private ImageView ttsButton;
+    TextToSpeech t1;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -43,23 +54,60 @@ public class MainActivity extends AppCompatActivity {
 
         inputField = findViewById(R.id.inputTextField);
         outputField = findViewById(R.id.outputTextField);
-        translateButton = (Button)findViewById(R.id.translateButton);
+        translateButton = findViewById(R.id.translateButton);
+        ttsButton = findViewById(R.id.ttsButton);
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        ttsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = outputField.getText().toString();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+
         translateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
+                if (!switchButton.isChecked()) {
+                    String input = inputField.getEditText().getText().toString();
+                    String url = "http://10.0.2.2:8080/getDescription?emojiIcon=" + input;
+                    new MyTask().execute(url);
+                } else {
+                    String input = inputField.getEditText().getText().toString();
+                    String url = "http://10.0.2.2:8080/getTagMatches?tags=" + input;
+                    new MyTask().execute(url);
 
-                String input = inputField.getEditText().getText().toString();
-                String url = "http://10.0.2.2:8080/getDescription?emojiIcon=" + input;
-                new MyTask().execute(url);
+                }
 
+            }
+        });
+
+        switchButton = findViewById(R.id.switch_button);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    inputField.setHint("Description");
+                    outputField.setHint("Emoji");
+                } else {
+                    inputField.setHint("Emoji");
+                    outputField.setHint("Description");
+                }
             }
         });
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
@@ -89,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         protected void onPostExecute(String result) {
-            outputField.setText(result);
+            outputField.setText(StringEscapeUtils.unescapeJava(result));
         }
     }
 }
