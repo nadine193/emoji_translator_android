ce919e3fb1d69cf9322c6615f4bec6add0078a1c
nadine193 <nadine.el-refee@student.manchester.ac.uk>
2020-03-10 01:58:20 +0000

    speech to text implemented speech returns emojis

--- src/main/java/com/example/emojitranslator/MainActivity.java
+++ src/main/java/com/example/emojitranslator/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.emojitranslator;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -27,17 +30,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int REQ_CODE = 100;
     private Toolbar mToolbar;
     private TextInputLayout inputField;
     private EditText outputField;
     private Button translateButton;
     private ToggleButton switchButton;
     private ImageView ttsButton;
+    private ImageView sttButton;
+    private ListView listView;
     TextToSpeech t1;
+    private List<String> result;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -56,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         outputField = findViewById(R.id.outputTextField);
         translateButton = findViewById(R.id.translateButton);
         ttsButton = findViewById(R.id.ttsButton);
+        sttButton = findViewById(R.id.sttButton);
         t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -72,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
             }
         });
+
 
 
         translateButton.setOnClickListener(new View.OnClickListener() {
@@ -103,8 +114,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+        sttButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry your device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String words = result.get(0);
+                    String url = "http://10.0.2.2:8080/getTagMatches?tags=" + words;
+                    new MyTask().execute(url);
+                }
+                break;
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -140,4 +184,5 @@ public class MainActivity extends AppCompatActivity {
             outputField.setText(StringEscapeUtils.unescapeJava(result));
         }
     }
+
 }
